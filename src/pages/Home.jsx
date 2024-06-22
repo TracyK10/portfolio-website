6e12d589e26618761a4202ebda35a497d11082e9
Tracy Karanja <chirutracy@gmail.com>
2024-06-22 10:41:31 +0300

    new commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,29 +27,35 @@ function Home() {
       </header>
       <main className="hero-section flex justify-between ml-28 mr-10 mt-32">
         <img
-          src="/src/assets/hero-image.png"
+          src="https://assets-global.website-files.com/603c87adb15be3cb0b3ed9b5/662b1f8a4628a60fe7a4b2f1_006-p-800.png"
           alt="Hero"
           className="hero-image"
         />
         <div>
           <h2>Hello, I am</h2>
-          <h1 className="text-amber-900" id="typewriter-name" ref={typewriterRef}></h1>
+          <h1
+            className="text-amber-900"
+            id="typewriter-name"
+            ref={typewriterRef}
+          ></h1>
           <h3 className="description py-3 w-3/4">
             Currently a student developer at Moringa and SheCodes. I have a
             passion for creating innovative and efficient projects. I am looking
             forward to continuous learning and innovation in the tech industry.
           </h3>
           <Link to="/contact">
-            <button className="rounded-full drop-shadow-lg">Get in touch</button>
+            <button className="rounded-full drop-shadow-lg">
+              Get in touch
+            </button>
           </Link>
         </div>
       </main>
-            <div className="text-center mt-32 mb-32">
+      <div className="text-center mt-32 mb-32">
         <h2 className="text-amber-900">Want to see what I have worked on?</h2>
         <Link to="/projects">
           <button className="rounded-full drop-shadow-lg">View Projects</button>
         </Link>
-        </div>
+      </div>
       <div className="text-center mt-32 mb-32">
         <h2 className="text-amber-900">Looking to collaborate?</h2>
         <p className="text-amber-900">
