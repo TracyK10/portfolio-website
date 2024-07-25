fa9012ac16928472acbfb5cb51ad83270291502f
Tracy Karanja <chirutracy@gmail.com>
2024-07-25 10:28:04 +0300

    Added resposive design using tailwind

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,54 +9,73 @@ function NavBar() {
   };
 
   return (
-    <nav className="flex items-center justify-between p-4">
-      <img src={Logo} alt="my logo" className="h-12" />
-      <div className="md:hidden">
-        <button onClick={toggleMenu} className="text-2xl focus:outline-none">
-          &#9776;
-        </button>
-      </div>
-      <ul
-        className={`flex-col md:flex-row md:flex md:space-x-10 items-center ${
-          isOpen ? "flex" : "hidden"
-        }`}
-      >
-        <li>
-          <a href="#home" className="block py-2 md:py-0 text-xl font-semibold">
-            Home
-          </a>
-        </li>
-        <li>
-          <a
-            href="#projects"
-            className="block py-2 md:py-0 text-xl font-semibold"
-          >
-            Projects
-          </a>
-        </li>
-        <li>
-          <a href="#about" className="block py-2 md:py-0 text-xl font-semibold">
-            About
-          </a>
-        </li>
-        <li>
-          <a
-            href="#skills"
-            className="block py-2 md:py-0 text-xl font-semibold"
-          >
-            Skills
-          </a>
-        </li>
-        <li>
-          <a
-            href="#experience"
-            className="block py-2 md:py-0 text-xl font-semibold"
-          >
-            Experience
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className="flex items-center justify-between p-4 relative z-10">
+        <img src={Logo} alt="my logo" className="h-12" />
+        <div className="md:hidden">
+          <button onClick={toggleMenu} className="text-2xl focus:outline-none">
+            &#9776;
+          </button>
+        </div>
+        <ul
+          className={`${
+            isOpen ? "flex" : "hidden"
+          } flex-col md:flex md:flex-row md:space-x-10 items-center md:items-baseline absolute md:static top-16 left-0 w-full md:w-auto md:bg-transparent`}
+        >
+          <li className="block text-center md:text-left">
+            <a
+              href="#home"
+              className="block py-2 md:py-0 text-xl font-semibold"
+              onClick={toggleMenu}
+            >
+              Home
+            </a>
+          </li>
+          <li className="block text-center md:text-left">
+            <a
+              href="#projects"
+              className="block py-2 md:py-0 text-xl font-semibold"
+              onClick={toggleMenu}
+            >
+              Projects
+            </a>
+          </li>
+          <li className="block text-center md:text-left">
+            <a
+              href="#about"
+              className="block py-2 md:py-0 text-xl font-semibold"
+              onClick={toggleMenu}
+            >
+              About
+            </a>
+          </li>
+          <li className="block text-center md:text-left">
+            <a
+              href="#skills"
+              className="block py-2 md:py-0 text-xl font-semibold"
+              onClick={toggleMenu}
+            >
+              Skills
+            </a>
+          </li>
+          <li className="block text-center md:text-left">
+            <a
+              href="#experience"
+              className="block py-2 md:py-0 text-xl font-semibold"
+              onClick={toggleMenu}
+            >
+              Experience
+            </a>
+          </li>
+        </ul>
+      </nav>
+      {isOpen && (
+        <div
+          className="fixed inset-0 bg-lime-800 bg-opacity-90 opacity-90 z-0"
+          onClick={toggleMenu}
+        ></div>
+      )}
+    </>
   );
 }
 
